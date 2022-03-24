4b95ba1e883fa1474df1f836fea2118cef47ad4e
Brodie Frail <B00369676@studentmail.uws.ac.uk>
2022-03-24 13:36:17 +0000

    Added stuff

---
+++ [Working Title]/Assets/Scripts/Base_Class.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Base_Class : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public virtual void Activate() { }
+
+    public virtual void Deactivate() { }
+}
